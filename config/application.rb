f15972d8ba500e18027514ca043fc81b74ada112
Hang Xuka <phamhangmta94@gmail.com>
2016-06-22 16:43:04 +0700

    static page

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,10 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path("../boot", __FILE__)
 
-require 'rails/all'
+require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+  # Require the gems listed in Gemfile, including any gems
+  # you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module BaseApp
