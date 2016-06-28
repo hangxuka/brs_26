232137275b9d4703f04ed4004fc624ca388d55cf
Vu Dinh Hao <vudinhhao.framgia.com>
2016-06-28 11:11:21 +0700

    show-all-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,11 @@ class User < ActiveRecord::Base
     foreign_key: :followed_id, dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  private
+  def picture_size
+    if picture.size > Settings.users.pic_size.megabytes
+      errors.add(:avatar, I18n.t("models.book.validate_picture"))
+    end
+  end
 end
